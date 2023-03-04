3dbbe9438c40ea076ac76321b005501d589ec650
selimdilsad <selimdilsadercan@gmail.com>
2023-03-04 15:27:51 +0300

    hello

---
+++ tut.js
@@ -0,0 +1,5 @@
+// spacing: 10, 20, 40 olabilir, kareler arası boşluk
+// length: 1: noktalar, 5: ufak artılar, 10: grid view
+// colour: istediğimiz bir rgb kodu
+// snap: snapi açıp kapatma
+// zoom-controls: controlorü açıp kapatır
